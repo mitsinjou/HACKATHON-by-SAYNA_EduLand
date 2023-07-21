62ca20c4743d3d843dac98528d8e65cde601b97f
Zo Nirina <tomzonirina@gmail.com>
2023-07-21 23:19:46 +0300

    Demo of the root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      
+      <Link href="/Dashbord">Click here to go into the page other</Link>
     </main>
   )
 }
