8b1bf3fd6c1f2f466f1315ceecc204778a17096e
Zo Nirina <tomzonirina@gmail.com>
2023-07-22 22:13:30 +0300

    Adding navbar

---
+++ src/app/Components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar(){
+    return(
+        <nav className="fixed top-0 left-0 w-full p-4 bg-blue-500 text-white transition duration-500">
+            <p>Acceuil</p>
+        </nav>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image'
+
+import React, { useState, useEffect } from 'react'
+
+import Navbar from './Components/navbar'
+
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
       <header className='w-screen h-screen fristHeader'>
-
+        <Navbar/>
       </header>
       <main>
         <Link href="/Dashbord">Click here to go into the page other</Link>
