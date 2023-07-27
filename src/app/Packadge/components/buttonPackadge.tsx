837d7a04647fecb96646ee507574fcce8cb48cd7
Zo Nirina <tomzonirina@gmail.com>
2023-07-27 21:59:29 +0300

    I have finish the packadge page

--- src/app/Packadge/components/buttonPackadge.tsx
+++ src/app/Packadge/components/buttonPackadge.tsx
@@ -3,7 +3,7 @@ import React from'react'
 export default function ButtonPackadge(){
     return(
         <>
-            <button type="button" className="absolute top-[-8%] left-[45%] text-center text-white text-lg font-normal pl-[15px] pr-4 py-[13px] bg-red-600 rounded-lg justify-center items-center inline-flex">
+            <button type="button" className="absolute top-[-8%] left-[44%] text-center text-white text-lg font-normal pl-[15px] pr-4 py-[13px] bg-red-600 rounded-lg justify-center items-center inline-flex">
                 Le plus populaire
             </button>
         </>
--- src/app/Packadge/components/mapPackadge.tsx
+++ src/app/Packadge/components/mapPackadge.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
+import { FaStar } from "react-icons/fa6"
+
 
 const DataCards = [
     {
@@ -11,7 +13,14 @@ const DataCards = [
         paraFirst: 'Plan Transport',
         btn: 'Select',
         price: '140.000Ar/Intervenant',
-        paraSecond: 'Organisation du transport'
+        paraSecond: 'Organisation du transport',
+        paradesc: 'Billet',
+        spandesc: 'Départ - Arrivé',
+        paradescone: 'Repas',
+        spandescone: 'Pendant le voyage',
+        fastar: ``,
+        paradesctwo: '',
+        spandestwo: ''
     },
     {
         id: 2,
@@ -22,7 +31,14 @@ const DataCards = [
         paraFirst: 'Plan Evenementiel',
         btn: 'Select',
         price: '200.000Ar(*)',
-        paraSecond: "Organisation de l'événement"
+        paraSecond: "Organisation de l'événement",
+        paradesc: 'Salle',
+        spandesc: "de l'évènement",
+        paradescone: 'Matériel',
+        spandescone: '(Son et Logistique)',
+        fastar: ``,
+        paradesctwo: '',
+        spandestwo: ''
     },
     {
         id: 3,
@@ -33,35 +49,42 @@ const DataCards = [
         paraFirst: 'Plan Séjour',
         btn: 'Select',
         price: '400.000Ar(**)',
-        paraSecond: "Organisation du séjour"
+        paraSecond: "Organisation du séjour",
+        paradesc: 'Voyage',
+        spandesc: '(Transport)',
+        paradescone: 'Hebergement',
+        spandescone: '(Hotel)',
+        fastar: <FaStar/>,
+        paradesctwo: 'Evenement',
+        spandestwo: '(Organisation)'
     }
 ]
 
 
 export default function MapPackadge(){
 
-    const [isMouseOver, setIsMouseOver] = useState(false);
+    const [isMouseOver, setIsMouseOver] = useState(-1);
 
-    const handleMouseEnter = () => {
-        setIsMouseOver(true);
+    const handleMouseEnter = (index: number) => {
+        setIsMouseOver(index);
     }
 
     const handleMouseLeave = () => {
-        setIsMouseOver(false);
+        setIsMouseOver(-1);
     }
 
     return(
         <>
-            {DataCards.map(DataCard => <section className='w-[363px] h-[431px] rounded-lg shadow border border-stone-400' key={DataCard.id}>
-                    <section className={`w-[363px] h-[215.5px] justify-center items-center ${isMouseOver ? 'hidden' : 'flex'}`}>
-                        <div className='w-[182px] h-[182px] bg-zinc-300 justify-center items-center rounded-full flex shadow' onMouseEnter={handleMouseEnter}>
+            {DataCards.map((DataCard, index) => <section className='w-[363px] h-[431px] rounded-lg shadow border border-stone-400' key={DataCard.id}>
+                    <section className={`w-[363px] h-[215.5px] justify-center items-center ${isMouseOver === index ? 'hidden' : 'flex'}`}>
+                        <div className='w-[182px] h-[182px] bg-zinc-300 justify-center items-center rounded-full flex shadow' onMouseEnter={() => handleMouseEnter(index)}>
                             <Image src={DataCard.icon} alt={DataCard.iconAlt} width={124} height={124} />
                         </div>
                     </section>
-                    <section className={`w-[363px] h-[215.5px] ${isMouseOver ? 'block' : 'hidden'}`}  onMouseLeave={handleMouseLeave}>
-                        <Image src={DataCard.img} alt={DataCard.imgAlt} width={363} height={215.5} />
+                    <section className={`w-[363px] h-[215.5px] ${isMouseOver === index ? 'block' : 'hidden'}`}  onMouseLeave={handleMouseLeave}>
+                        <Image src={DataCard.img} alt={DataCard.imgAlt} className='rounded-t-lg' width={363} height={215.5} />
                     </section>
-                    <article className={`w-[363px] h-[215.5px] grid-row-2 ${ isMouseOver ? 'hidden' : 'grid' }`}>
+                    <article className={`w-[363px] h-[215.5px] grid-row-2 ${ isMouseOver === index ? 'hidden' : 'grid' }`}>
                         <article className="flex justify-center items-center">
                             <p className="text-black font-sans text-lg font-semibold">{DataCard.paraFirst}</p>
                         </article>
@@ -72,10 +95,24 @@ export default function MapPackadge(){
                             <p className='text-black text-lg font-normal underline'>{DataCard.price}</p>
                         </section>
                     </article>
-                    <article className={`w-[363px] h-[215.5px] justify-center items-center ${isMouseOver ? 'flex' : 'hidden'}`}>
-                        <p className="w-[300px] h-[87px] text-center text-black text-lg font-normal leading-[38px]">
+                    <article className={`w-[363px] h-[215.5px] justify-center items-start flex-col ${isMouseOver === index ? 'flex' : 'hidden'}`}>
+                        <p className="w-[300px] h-[87px] flex items-start justify-center text-black text-lg font-normal leading-[38px]">
                             {DataCard.paraSecond}
                         </p>
+                        <section className='w-[363px] h-24'>
+                            <p className='flex justify-center items-center h-6'>
+                                <FaStar/>
+                                {DataCard.paradesc} <span>{DataCard.spandesc}</span>   
+                            </p>
+                            <p className='flex justify-center items-center my-1 h-6'>
+                                <FaStar/>
+                                {DataCard.paradescone} <span>{DataCard.spandescone}</span>
+                            </p>
+                            <p className='flex justify-center items-center h-6'>
+                                {DataCard.fastar}
+                                {DataCard.paradesctwo} <span> {DataCard.spandestwo} </span>
+                            </p>
+                        </section>
                     </article>
             </section>)}
         </>
