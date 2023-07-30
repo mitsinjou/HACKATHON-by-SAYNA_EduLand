38f1d4bdd84efe7a21da5aa12500068b124c1caa
Zo Nirina <tomzonirina@gmail.com>
2023-07-30 23:53:37 +0300

    I finish the responsive of the company page

--- src/app/Partenaire/page.tsx
+++ src/app/Partenaire/page.tsx
@@ -5,9 +5,9 @@ import PartenaryMap from './components/partenaryMap'
 export default function Page(){
     return(
         <>
-            <header className='w-screen h-[230vh] flex justify-center items-start relative secondHeader'>
+            <header className='w-screen h-[500vh] lg:h-[230vh] flex justify-center items-start relative secondHeader'>
                 <SecondNavbar/>
-                <section className='w-screen h-[200vh] pt-[40vh] px-8 flex justify-center flex-wrap gap-6 mb-16'>
+                <section className='w-screen h-[400vh] lg:h-[200vh] pt-[40vh] px-8 flex justify-center flex-wrap gap-6 mb-16'>
                     <PartenaryMap/>
                 </section>
             </header>
