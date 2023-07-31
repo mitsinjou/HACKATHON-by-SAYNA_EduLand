3611c4d1a68cc60de1b3b11aaa579509650dc60f
Zo Nirina <tomzonirina@gmail.com>
2023-07-31 22:09:31 +0300

    I finished the animation in the hero page

--- src/app/Components/hero.tsx
+++ src/app/Components/hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import { useFormik } from 'formik'
 import { FaMagnifyingGlass } from 'react-icons/fa6'
+import { motion } from 'framer-motion'
 
 export default function Hero(){
     const formik = useFormik({
@@ -14,14 +15,14 @@ export default function Hero(){
     });
     return(
         <section className='w-[80%] h-[70vh] grid grid-rows-2 lg:grid lg:grid-cols-2'>
-            <article className='grid h-[70vh] z-[2] lg:z-0 content-evenly'>
+            <motion.article initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }} className='grid h-[70vh] z-[2] lg:z-0 content-evenly'>
                 <p className='text-justify text-orange-400 text-[24px] lg:text-[36px] font-semibold'>Nous vous aidons à proposer des formations de qualités partout à Madagascar</p>
                 <p className='text-justify text-white text-base lg:text-xl font-semibold'>Eduland vous offre des services logistiques adaptés à vos besoins. Pour vous mettre dans des bonnes conditions et vous permettre de proposer des formations de qualité partout à Madagascar</p>
                 <Link href="#contact">
                     <button type='button' className='p-2.5 hover:bg-orange-400 bg-red-600 rounded-lg shadow justify-center items-center gap-2.5 inline-flex text-white text-base lg:text-lg font-semibold uppercase'>Demandez un devis</button>
                 </Link>
-            </article>
-            <section className='flex h-[70vh] items-end justify-center'>
+            </motion.article>
+            <motion.section initial={{ opacity: 0, x: 100 }} whileInView={{ opacity: 1, x: 0 }} className='flex h-[70vh] items-end justify-center'>
                 <form onSubmit={formik.handleSubmit} className='flex flex-col lg:flex-row mb-8'>
                 <div className='relative'>
                 <FaMagnifyingGlass className='absolute pointer-events-none top-[30%] left-[2%] w-5 h-5'/>
@@ -37,7 +38,7 @@ export default function Hero(){
                 </div>    
                 <button type="submit" className='mx-4 my-4 lg:my-0 p-2.5 hover:bg-orange-400 bg-red-600 rounded-lg shadow justify-center items-center gap-2.5 inline-flex text-white text-lg font-semibold'>Rechercher</button>
                 </form>
-            </section>
+            </motion.section>
         </section>
     )
 }
